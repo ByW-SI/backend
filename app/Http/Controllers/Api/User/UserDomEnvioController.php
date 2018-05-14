ab099693ea09d84262ef3fd901ad3a40e95bcd6d
Ivan Rojo <ivanrojo07@gmail.com>
2018-05-14 15:57:18 -0500

    Domicilio Envio Controller

--- app/Http/Controllers/Api/User/UserDomEnvioController.php
+++ app/Http/Controllers/Api/User/UserDomEnvioController.php
@@ -13,9 +13,21 @@ class UserDomEnvioController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
         //
+        $user = $request->user();
+        // dd($user);
+        $domicilios = $user->domEnvio;
+        // return response()->json(['domicilio' => $domicilios],200);
+        if ($domicilios == null) {
+            # code...
+            return response()->json(['message'=>"No hay registro"],200);
+        }
+        else{
+            
+            return response()->json(['domicilio' => $domicilios],200);
+        }
     }
 
     
@@ -29,6 +41,42 @@ class UserDomEnvioController extends Controller
     public function store(Request $request)
     {
         //
+        $user = $request->user();
+        $data = $request->all();
+        if ($user->domEnvio == null) {
+            # code...
+             // Validation datas
+            $rules = [
+                "pais" => "required",
+                "estado" => "required",
+                "municipio" => "required",
+                "ciudad" => "required",
+                "colonia" => "required",
+                "calle" => "required",
+                "numext" => "required"
+            ];
+            $this->validate($request, $rules);
+
+            // Create Domicilio Fiscal
+            $domicilio = DomEnvio::create([
+                "user_id" => $user->id,
+                "pais" => $data['pais'],
+                "estado" => $data['estado'],
+                "municipio" => $data['municipio'],
+                "ciudad" => $data['ciudad'],
+                "colonia" => $data['colonia'],
+                "calle" => $data['calle'],
+                "numext" => $data['numext'],
+                "numint" => ($data['numint'] == null ? "1" : $data['numint'] )
+            ]);
+
+            return response()->json([$domicilio],200);
+
+        }
+        else{
+            
+            return response()->json(['message'=>"Ya tienes tu dirección fiscal"],200);
+        }
     }
 
     /**
@@ -37,9 +85,21 @@ class UserDomEnvioController extends Controller
      * @param  \App\DomEnvio  $domEnvio
      * @return \Illuminate\Http\Response
      */
-    public function show(DomEnvio $domEnvio)
+    public function show(Request $request, DomEnvio $domEnvio)
     {
         //
+        $user = $request->user();
+        // dd($user);
+        $domicilio = $user->domEnvio->where('id', $domEnvio);
+
+        if ($domicilio == null) {
+            # code...
+            return response()->json(['message'=>"No hay registro"],200);
+        }
+        else{
+            
+            return response()->json(['domicilio' => $domicilio],200);
+        }
     }
 
 
@@ -53,6 +113,43 @@ class UserDomEnvioController extends Controller
     public function update(Request $request, DomEnvio $domEnvio)
     {
         //
+        $user = $request->user();
+        // dd($user);
+        $domicilio = $user->domEnvio->where('id', $domEnvio);
+        $data = $request->all();
+        // dd($request->all());
+        // dd($domicilio);
+        if ($domicilio == null) {
+
+            return response()->json(['message'=>"Necesitas crear una direccion fiscal"],200);
+        }
+        else{
+
+            $rules = [
+                "pais" => "required",
+                "estado" => "required",
+                "municipio" => "required",
+                "ciudad" => "required",
+                "colonia" => "required",
+                "calle" => "required",
+                "numext" => "required"
+            ];
+            $this->validate($request, $rules);
+
+            $domicilio->update([
+                "pais" => $data['pais'],
+                "estado" => $data['estado'],
+                "municipio" => $data['municipio'],
+                "ciudad" => $data['ciudad'],
+                "colonia" => $data['colonia'],
+                "calle" => $data['calle'],
+                "numext" => $data['numext'],
+                "numint" => ($data['numint'] == null ? "1" : $data['numint'] )
+            ]);
+            return response()->json([$domicilio],200);
+
+        }
+
     }
 
     /**
@@ -61,8 +158,19 @@ class UserDomEnvioController extends Controller
      * @param  \App\DomEnvio  $domEnvio
      * @return \Illuminate\Http\Response
      */
-    public function destroy(DomEnvio $domEnvio)
+    public function destroy(Request $request, DomEnvio $domEnvio)
     {
         //
+        $user = $request->user();
+        // dd($user);
+        $domicilio = $user->domEnvio->where('id', $domEnvio);
+        if ($domicilio == null) {
+
+            return response()->json(['message'=>"Necesitas crear una direccion fiscal"],200);
+        }
+        else{
+            $domicilio->delete();
+            return response()->json(['message'=>"Direccion fiscal eliminada con éxito"],200);
+        }
     }
 }
