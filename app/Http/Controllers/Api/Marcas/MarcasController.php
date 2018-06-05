0dc147184336601eceaf29daf218c056819d5583
Ivan Rojo <ivanrojo07@gmail.com>
2018-06-05 14:10:46 -0500

    marcas controller in api

---
+++ app/Http/Controllers/Api/Marcas/MarcasController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers\Api\Marcas;
+
+use App\Marca;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class MarcasController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        $marcas = Marca::get();
+        return response()->json(['marcas'=>$marcas],202);
+
+    }
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Marca  $marca
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Marca $marca)
+    {
+        //
+        return response()->json(['marca'=>$marca],202);
+
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -25,6 +25,7 @@ Route::middleware('auth:api')->resource('domicilios', 'Api\User\UserDomEnvioCont
 Route::middleware('auth:api')->resource('cards', 'Api\User\UserTarjetasController', ['except'=>['create','update','edit']]);
 Route::resource('uvas', 'Api\Uva\UvaController',['only'=>'index']);
 Route::resource('vinicolas','Api\Vinicola\VinicolaController',['only'=>'index']);
+Route::resource('marcas','Api\Marcas\MarcasController',['only'=>['index','show']]);
 // Route::middleware('auth:api')->put('fiscales', 'Api\User\UserDomFiscalController@update');
 
 Route::post('login', 'User\UserController@login')->name('login');
