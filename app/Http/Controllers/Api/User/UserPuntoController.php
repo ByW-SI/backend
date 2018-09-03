e1cd6a87ff1577c6fae7b696cac3466e0ff27f94
Ivan Rojo <ivanrojo07@gmail.com>
2018-09-03 11:15:12 -0500

    restricción de no crear un nuevo codigo si ya existe uno

--- app/Http/Controllers/Api/User/UserPuntoController.php
+++ app/Http/Controllers/Api/User/UserPuntoController.php
@@ -36,7 +36,8 @@ class UserPuntoController extends Controller
     {
         //
         $user=$request->user();
-        $last_punto = Punto::last();
+        $last_punto = Punto::orderBy('expira', 'desc')->first();
+        // dd($last_punto->isExpired());
         if ($last_punto->isExpired()) {
             $punto= Punto::create([
                 'user_id'=>$user->id,
